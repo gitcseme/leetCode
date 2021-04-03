a95ae2db91f338270a8371b7bdd512ad8cb88222
kashuvo <shuvo_CSE23@yahoo.com>
2021-04-03 21:56:35 +0600

    Sqrt(x) should be sqrt(x + 0.01)

---
+++ Sqrt_x.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+#define all(ar) ar.begin(), ar.end()
+#define FI freopen ("inp.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+#define FOR(v) for(auto i : v) {cout << i << " ";} cout << "\n";
+#define FIRSTIO ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        double sqx = x + 0.001;
+        int ans = sqrt(sqx);
+        return ans;
+    }
+};
+
+static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL); return 0; }();
+
+int main() {
+    //FI;
+    Solution sln;
+    cout << sln.mySqrt(0) << "\n";
+
+    return 0;
+}
+
