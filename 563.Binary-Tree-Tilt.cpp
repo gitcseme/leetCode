c63c4bca11168cf1ee3017cfa15689c96a992159
kashuvo <shuvo_CSE23@yahoo.com>
2021-12-08 20:47:16 +0600

    simple tree traversal

---
+++ 563.Binary-Tree-Tilt.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define ff first
+#define ss second
+#define nline "\n"
+#define ll long long
+#define all(ar) ar.begin(), ar.end()
+#define FI freopen ("inp.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+#define FASTIO ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define debug(args...){string _s=#args;replace(_s.begin(),_s.end(),',',' ');stringstream _ss(_s);istream_iterator<string> _it(_ss);err(_it,args);}
+void err(istream_iterator<string> it) {}
+template<typename T,typename... Args> void err(istream_iterator<string> it,T a,Args... args){cerr<<*it<<" = "<<a<<"\n";err(++it,args...);}
+template<typename T> void _print(T _t){cout<<"[ ";for(auto _x:_t){cout<<_x<<" ";}cout<<" ]\n";}
+template<class T> void _print(map<T, T> x){for(auto i:x)cout<<i.ff<<" "<<i.ss<<"\n";}
+/*---------------------------Codes-----------------------------*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        int tilt = 0;
+        solve(root, tilt);
+        return tilt;
+    }
+    
+    int solve(TreeNode* root, int& tilt) {
+        if (root == nullptr) 
+            return 0;
+
+        if (root->left == nullptr && root->right == nullptr) 
+            return root->val;
+
+        int lnode = solve(root->left, tilt);
+        int rnode = solve(root->right, tilt);
+
+        tilt += abs(lnode - rnode);
+
+        return (root->val + lnode + rnode);
+    }
+};
+
+// LEET CODE
+static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL); return 0; }();
+
+int main() {
+    FASTIO;
+#ifdef DEBUG_LOCAL
+    FI;
+#endif
+    
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(5);
+
+    root->right = new TreeNode(9);
+    root->right->right = new TreeNode(7);
+    
+    Solution sln;
+    cout << sln.findTilt(root);
+
+    return 0;
+}
