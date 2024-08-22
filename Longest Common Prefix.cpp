781d43eb2850d19cda67c142283fc81696727133
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-08-22 11:42:29 +0600

    easy code [tjip]

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -1,48 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ff first
-#define ss second
-#define all(ar) ar.begin(), ar.end()
-#define FI freopen ("inp.txt", "r", stdin)
-#define FO freopen ("out.txt", "w", stdout)
-#define FOR(v) for(int i : v) {cout << i << " ";} puts("");
-#define FIRSTIO ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
-
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string lcp = "";
-        if (strs.size() == 0) return lcp;
-
-        int j = 0;
-        while (1) {
-            if (!(j < strs[0].length()))
-                break;
-            char c = strs[0][j];
-
-            bool match = true;
-            for (int i = 0; i < strs.size(); ++i) {
-                if (strs[i].length()==0 || strs[i].length() < j || strs[i][j] != c) {
-                    match = false;
-                    break;
-                }
-            }
-
-            if (!match) break;
-            else {
-                lcp += c;
-                ++j;
-            }
-        }
-        return lcp;
+string longestCommonPrefix(vector<string>& strs) {
+    int min_length = INT_MAX;
+    for (auto &s : strs) {
+        min_length = min(min_length, (int)s.size());
     }
-};
+    
+    int lcp = 0, n = strs.size();
+    for (int i = 0; i < min_length; ++i) {
+        bool match = true;
+        for (int j = 0; j + 1 < n; ++j) {
+            match = match & (strs[j][i] == strs[j+1][i]);
+        }
 
-int main() {
-    //FI;
-    Solution sln;
-    vector<string> vec = {"a"};
-    cout << sln.longestCommonPrefix(vec) << "\n";
+        if (!match) break;
+        ++lcp;
+    }
 
-    return 0;
+    return strs[0].substr(0, lcp);
 }
