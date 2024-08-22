4fcf1d798e481b3a07df8688be79aa4e18de048d
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-08-23 02:25:12 +0600

    find anagram groups using hash

--- Group Anagrams.cpp
+++ Group Anagrams.cpp
@@ -1,44 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ff first
-#define ss second
-#define all(ar) ar.begin(), ar.end()
-#define FI freopen ("inp.txt", "r", stdin)
-#define FO freopen ("out.txt", "w", stdout)
-#define FOR(v) for(auto i : v) {cout << i << " ";} cout << "\n";
-#define FIRSTIO ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
-
 class Solution {
 public:
+    long long calculateHash(string& str) {
+        long long H = 1;
+        static long long BASE = 789;
+        static long long MOD = 1000000000 + 7;
+
+        for (char& c : str) {
+            H = (H * (BASE + c)) % MOD;
+        }
+
+        return H;
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string>> groups;
-        string key;
-        for (int i = 0; i < strs.size(); ++i) {
-            key = strs[i];
-            sort(key.begin(), key.end());
-            groups[key].push_back(strs[i]);
+        unordered_map<long long, vector<string>> dict;
+
+        for (auto& str : strs) {
+            long long h = calculateHash(str);
+            dict[h].push_back(str);
         }
 
-        vector<vector<string>> res;
-        for (auto it = groups.begin(); it != groups.end(); ++it) {
-            res.push_back(it->ss);
+        vector<vector<string>> grps;
+        for (auto it = dict.begin(); it != dict.end(); ++it) {
+            grps.push_back(it->second);
         }
 
-        return res;
+        return grps;
     }
 };
 
-static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL); return 0; }();
 
-int main() {
-    //FI;
-    Solution sln;
-    vector<string> vec = {"eat","tea","tan","ate","nat","bat"};
-    auto ans = sln.groupAnagrams(vec);
-    for (auto v : ans) {
-        FOR(v);
+
+//-> Normal solution
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    map<string, vector<string>> groups;
+    string key;
+    for (int i = 0; i < strs.size(); ++i) {
+        key = strs[i];
+        sort(key.begin(), key.end());
+        groups[key].push_back(strs[i]);
     }
 
-    return 0;
-}
+    vector<vector<string>> res;
+    for (auto it = groups.begin(); it != groups.end(); ++it) {
+        res.push_back(it->ss);
+    }
 
+    return res;
+}
