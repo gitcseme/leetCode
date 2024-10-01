8ee82f6f11113ccdb019c1f6e469174e30a8bdbe
Md. Kawsarul Alam Shuvo <shuvo.official2021@gmail.com>
2024-10-01 10:45:45 +0600

    in-order traversal actually process nodes in sorted order

--- 230. Kth Smallest Element in a BST.cpp
+++ 230. Kth Smallest Element in a BST.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    vector<int> nodes;
-    int kthSmallest(TreeNode* root, int k) {
-        preOrder(root, k);
-        return nodes[k-1];
-    }
-    
-    void preOrder(TreeNode* root, int k) {
-        if (root == nullptr) 
-            return;
+    int elem = -1;
+    bool found = false;
+
+    void dfs(TreeNode* root, int& k) {
+        if (root == nullptr || found) return;
+
+        dfs(root->left,  k);
         
-        preOrder(root->left, k-1);
-        nodes.push_back(root->val);
-        preOrder(root->right, k-1);
+        --k;
+        if (!found && k == 0) {
+            elem = root->val;
+            found = true;
+            return;
+        }
+
+        dfs(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        dfs(root, k);
+        return elem;
     }
 };
